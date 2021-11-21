6b59f22e414c58298314ac50f19da505be6d1056
Skyler Basco <58148243+skylerbasco@users.noreply.github.com>
2021-11-21 00:27:01 -0500

    Add files via upload

---
+++ Hello World App/Hello World App/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Hello World App
+//
+//  Created by Skyler on 11/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var TextLabel: UILabel!
+    @IBAction func ButtonClicked(_ sender: Any) {
+        print("hello")
+        TextLabel.textColor = UIColor.orange
+    }
+    
+}
+
---
+++ Hello World App/Hello World AppUITests/Hello_World_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Hello_World_AppUITestsLaunchTests.swift
+//  Hello World AppUITests
+//
+//  Created by Skyler on 11/20/21.
+//
+
+import XCTest
+
+class Hello_World_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
